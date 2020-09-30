4e41c73a8895eba807a95f15d0e6c8c711396298
Paola Garbato <paolagarbato@gmail.com>
2020-09-30 18:28:38 -0300

    Exibindo detalhes

--- huntweb/src/pages/main/index.js
+++ huntweb/src/pages/main/index.js
@@ -17,15 +17,15 @@ export default class Main extends Component {
 
     loadProducts = async (page = 1) => {
         const response = await api.get(`/products?page=${page}`);
-        const {docs, ...productInfo } = response.data;
+        const { docs, ...productInfo } = response.data;
 
-        this.setState({products: docs, productInfo, page});
+        this.setState({ products: docs, productInfo, page });
     };
 
     prevPage = () => {
         const { page, productInfo } = this.state;
 
-        if (page == 1) return;
+        if (page === 1) return;
 
         const pageNumber = page - 1;
 
@@ -35,7 +35,7 @@ export default class Main extends Component {
     nextPage = () => {
         const { page, productInfo } = this.state;
 
-        if (page == productInfo.pages) return;
+        if (page === productInfo.pages) return;
 
         const pageNumber = page + 1;
 
@@ -55,10 +55,10 @@ export default class Main extends Component {
                        <Link to={`/products/${product._id}`}>Acessar</Link>
                    </article>
                 ))}
-            <div className="actions">
-                <button disabled={page == 1} onClick={this.prevPage}>Anterior</button>
-                <button disabled={page == productInfo.pages} onClick={this.nextPage}>Próxima</button>
-            </div>
+                <div className="actions">
+                    <button disabled={page === 1} onClick={this.prevPage}>Anterior</button>
+                    <button disabled={page === productInfo.pages} onClick={this.nextPage}>Próxima</button>
+                </div>
             </div>
         );
     }
--- huntweb/src/routes.js
+++ huntweb/src/routes.js
@@ -1,17 +1,17 @@
 import React from 'react';
 
-import { BrownserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Main from './pages/main';
 import Product from './pages/product'
 
 const Routes = () => (
-    <BrownserRouter>
+    <BrowserRouter>
         <Switch>
             <Route exact path="/" component={Main} />
             <Route path="/products/:id" component={Product} />
         </Switch>
-    </BrownserRouter>
+    </BrowserRouter>
 )
 
 export default Routes;
