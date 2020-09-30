949fe79566f0c9439e9430a7a7715b960de50d6b
Paola Garbato <paolagarbato@gmail.com>
2020-09-30 17:38:55 -0300

    Consumindo outras infos da API e exibindo na próxima página

--- huntweb/src/pages/main/index.js
+++ huntweb/src/pages/main/index.js
@@ -5,17 +5,34 @@ import './styles.css';
 export default class Main extends Component {
     state = {
         products: [],
+        productInfo: {},
+        page: 1,
     }
 
     componentDidMount() {
         this.loadProducts();
     }
 
-    loadProducts = async () => {
-        const response = await api.get("/products");
+    loadProducts = async (page = 1) => {
+        const response = await api.get(`/products?page=${page}`);
+        const {docs, ...productInfo } = response.data;
 
-        this.setState({products: response.data.docs});
+        this.setState({products: docs, productInfo});
     };
+
+    prevPage = () => {
+
+    }
+
+    nextPage = () => {
+        const { page, productInfo } = this.state;
+
+        if (page == productInfo.pages) return;
+
+        const pageNumber = page + 1;
+
+        this.loadProducts(pageNumber);
+    }
 
     render() {
         const { products } = this.state;
@@ -29,9 +46,11 @@ export default class Main extends Component {
                        <p>{product.description}</p>
                        <a href="">Acessar</a>
                    </article>
-                    )
-                )
-            }
+                ))}
+            <div className="actions">
+                <button onClick={this.prevPage}>Anterior</button>
+                <button onClick={this.nextPage}>Próxima</button>
+            </div>
             </div>
         );
     }
